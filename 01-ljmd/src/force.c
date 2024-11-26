3979f2e907c90866b109fed21a22cebe0f2c13e4
Giacomo <giacomozuccarino@gmail.com>
2024-11-26 12:02:15 +0100

    initial version for combined serial and MPI, NOT WORKING for np >= 2

--- 01-ljmd/src/force.c
+++ 01-ljmd/src/force.c
@@ -2,7 +2,9 @@
 #include "mdsys.h"
 #include "utilities.h"
 #include "force.h"
-#include <mpi.h>
+#if defined(_MPI)
+    #include <mpi.h>
+#endif
 
 /* compute forces */
 void force(mdsys_t *sys)
@@ -11,9 +13,11 @@ void force(mdsys_t *sys)
     double rx,ry,rz;
     int i,j;
 
-    int rank,size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    # if defined(_MPI)
+        int rank,size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+    #endif
 
     /* zero energy and forces */
     sys->epot=0.0;
@@ -22,18 +26,23 @@ void force(mdsys_t *sys)
     azzero(sys->fz,sys->natoms);
 
     // MPI instruction - set auxiliary data to 0
-    double epot = 0.0;
-    azzero(sys->cx,sys->natoms);
-    azzero(sys->cy,sys->natoms);
-    azzero(sys->cz,sys->natoms);
+    #if defined(_MPI)
+        double epot = 0.0;
+        azzero(sys->cx,sys->natoms);
+        azzero(sys->cy,sys->natoms);
+        azzero(sys->cz,sys->natoms);
+    #endif
 
     // MPI instruction - broadcats particles positions from process of rank 0 to other ranks
-    MPI_Bcast(sys->rx, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(sys->ry, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(sys->rz, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    #if defined(_MPI)
+        MPI_Bcast(sys->rx, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(sys->ry, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(sys->rz, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    #endif
 
     int ii;
 
+    #if defined(_MPI)
     for(i=0; i < (sys->natoms); i+=size) {
         ii = i +rank;
         if (ii > sys->natoms) {
@@ -72,6 +81,34 @@ void force(mdsys_t *sys)
     MPI_Reduce(sys->cz, sys->fz, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     // MPI instruction - sum potential energy contribution from all ranks to process of rank 0
     MPI_Reduce(&epot, &(sys->epot), 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    #elseif
+        for(i=0; i < (sys->natoms); ++i) {
+        for(j=0; j < (sys->natoms); ++j) {
+
+            /* particles have no interactions with themselves */
+            if (i==j) continue;
+
+            /* get distance between particle i and j */
+            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
+            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
+            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
+            r = sqrt(rx*rx + ry*ry + rz*rz);
+
+            /* compute force and energy if within cutoff */
+            if (r < sys->rcut) {
+                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
+                                         +6*pow(sys->sigma/r,6.0)/r);
+
+                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
+                                               -pow(sys->sigma/r,6.0));
+
+                sys->fx[i] += rx/r*ffac;
+                sys->fy[i] += ry/r*ffac;
+                sys->fz[i] += rz/r*ffac;
+            }
+        }
+    }
+    #endif
 }
 
 
--- 01-ljmd/src/main.c
+++ 01-ljmd/src/main.c
@@ -11,7 +11,9 @@
 #include <stdlib.h>
 #include <math.h>
 #include <sys/time.h>
-#include <mpi.h>
+#if defined(_MPI)
+    #include <mpi.h>
+#endif
 
 /* Include version number */
 #include "LJMDConfig.h"
@@ -29,10 +31,12 @@
 int main(int argc, char **argv)
 {   
     // MPI instruction - set up mpi
-    MPI_Init(&argc, &argv);
-    int rank,size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    # if defined(_MPI)
+        MPI_Init(&argc, &argv);
+        int rank,size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+    # endif
 
     int nprint, i;
     char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
@@ -46,6 +50,7 @@ int main(int argc, char **argv)
 
     /* read input file */
     // MPI instruction - only process of rank 0 reads from instructions file
+    #if defined(_MPI)
     if ( rank == 0 ) {
         if(get_a_line(stdin,line)) return 1;
         sys.natoms=atoi(line);
@@ -69,18 +74,43 @@ int main(int argc, char **argv)
         if(get_a_line(stdin,line)) return 1;
         nprint=atoi(line);
     }
-
+    #elseif
+        if(get_a_line(stdin,line)) return 1;
+        sys.natoms=atoi(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.mass=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.epsilon=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.sigma=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.rcut=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.box=atof(line);
+        if(get_a_line(stdin,restfile)) return 1;
+        if(get_a_line(stdin,trajfile)) return 1;
+        if(get_a_line(stdin,ergfile)) return 1;
+        if(get_a_line(stdin,line)) return 1;
+        sys.nsteps=atoi(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.dt=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        nprint=atoi(line);
+    #endif
+    
     // MPI instruction - broadcast parameters to all ranks
+    #if defined(_MPI)
     MPI_Bcast(&sys.natoms, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.nsteps, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.mass, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&sys.epsilon, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.epsilon, 1,#if defined(_MPI) MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.sigma, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.rcut, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sys.box, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&nprint, 1, MPI_INT, 0, MPI_COMM_WORLD);
-   
+    #endif
+
     /* allocate memory */
     sys.rx=(double *)malloc(sys.natoms*sizeof(double));
     sys.ry=(double *)malloc(sys.natoms*sizeof(double));
@@ -93,11 +123,14 @@ int main(int argc, char **argv)
     sys.fz=(double *)malloc(sys.natoms*sizeof(double));
     
     // MPI instruction - allocate memory for auxiliary storage
+    #if defined(_MPI)
     sys.cx=(double *)malloc(sys.natoms*sizeof(double));
     sys.cy=(double *)malloc(sys.natoms*sizeof(double));
     sys.cz=(double *)malloc(sys.natoms*sizeof(double));
+    #endif
 
     // MPI instruction - only process of rank 0 reads intial positions for particles
+    #if defined(_MPI)
     if (rank == 0) {
         /* read restart */
         fp=fopen(restfile,"r");
@@ -116,37 +149,49 @@ int main(int argc, char **argv)
             perror("cannot read restart file");
             return 3;
         }
-
-    } // end if for rank zero
-    
+    }
+    #elseif
+        /* read restart */
+        fp=fopen(restfile,"r");
+        if(fp) {
+            for (i=0; i<sys.natoms; ++i) {
+                fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+            for (i=0; i<sys.natoms; ++i) {
+                fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+            }
+            fclose(fp);
+            azzero(outputsys.fx, sys.natoms);
+            azzero(sys.fy, sys.natoms);
+            azzero(sys.fz, sys.natoms);
+        } else {
+            perror("cannot read restart file");
+            return 3;
+        }
+    #endif    
     /* initialize forces and energies.*/
     sys.nfi=0;
-    
     force(&sys);
-
-    // gz: auxiliary print
-    /*
-    if (rank == 0) {
-        print1DVector(sys.fx, sys.natoms);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (rank == 1) {
-        print1DVector(sys.fx, sys.natoms);
-    }
-    */
     ekin(&sys);
 
-    // MPI instruction - only process of rank 0 writes the results on files and on scree
+    // MPI instruction - only process of rank 0 writes the results on files and on screen
+    #if defined(_MPI)
     if (rank == 0) {
+        erg=fopen(ergfile,"w");
+        traj=fopen(trajfile,"w");        
+        printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
+        printf("     NFI            TEMP       instruction     EKIN                 EPOT              ETOT\n");
+        output(&sys, erg, traj);
+    }
+    printf("Startup time, for rank %d. : %10.3fs\n",rank ,wallclock()-t_start);
+    #elseif
         erg=fopen(ergfile,"w");
         traj=fopen(trajfile,"w");        
         printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
         printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
         output(&sys, erg, traj);
-    }
+    #endif
 
-    printf("Startup time, for rank %d. : %10.3fs\n",rank ,wallclock()-t_start);
-
+    
     /* reset timer */
     t_start = wallclock();
 
@@ -154,13 +199,20 @@ int main(int argc, char **argv)
     /* main MD loop */
     for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
-        /* write output, if requested */
+        /* write output, if re#if defined(_MPI)quested */
         // MPI instruction - only process of rank 0 is writing
+        #if defined(_MPI)
         if (rank == 0) {
             if ((sys.nfi % nprint) == 0) {
                 output(&sys, erg, traj);
             }
         }
+        #elseif
+            if ((sys.nfi % nprint) == 0) {
+                output(&sys, erg, traj);
+            }
+        #endif
+
         /* propagate system and recompute energies */
         velverlet1(&sys);
         force(&sys);
@@ -170,12 +222,18 @@ int main(int argc, char **argv)
     /**************************************************/
 
     /* clean up: close files, free memory */
-    printf("Simulation Done for rank %d. Run time: %10.3fs\n",rank,wallclock()-t_start);
+    
     // MPI instruction - process of rank 0 closes the files
-    if (rank == 0) { 
+    #if defined(_MPI)
+    printf("Simulation Done for rank %d. Run time: %10.3fs\n",rank,wallclock()-t_start);
+    if (rank == 0) {    
         fclose(erg);
         fclose(traj);
     }
+    #elseif
+        fclose(erg);
+        fclose(traj);
+    #endif
 
     free(sys.rx);
     free(sys.ry);
@@ -190,8 +248,9 @@ int main(int argc, char **argv)
     free(sys.cy);
     free(sys.cz);
     
-    //gz: end mpi
-    MPI_Finalize();
+    #if defined(_MPI) 
+        MPI_Finalize();
+    #endif
 
     return 0;
 }
